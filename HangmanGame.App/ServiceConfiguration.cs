859eb026a944b2a9597415065157d87e0883a61b
Sviatoslav Mandryk <sviatolsav.mandryk@just-eat.com>
2020-10-06 19:52:54 +0300

    added initial projects

---
+++ HangmanGame.App/ServiceConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HangmanGame.App
+{
+    internal class ServiceConfiguration
+    {
+        public IServiceProvider BuildServiceProvider(IConfiguration configuration)
+        {
+            var serviceCollection = GetServiceCollection(configuration);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+
+        private IServiceCollection GetServiceCollection(IConfiguration configuration)
+        {
+            var serviceCollection = new ServiceCollection()
+            {
+
+            };
+
+            return serviceCollection;
+        }
+
+        public IConfiguration BuildConfigurationProvider()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("config.json", false, false)
+                .Build();
+        }
+    }
+}
